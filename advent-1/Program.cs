90e73c9f85e921109a04eb3674e9e8a404751934
Simon Agozzino <simon@2a01cb0d06007c006eb664d391b79d4a.ipv6.abo.wanadoo.fr>
2025-12-05 16:59:34 +0100

    day 1

---
+++ advent-1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Pipelines;
+
+const int START_POSITION = 50;
+
+int dial = START_POSITION;
+int nbZero, stopZero, currentPosition;
+nbZero = stopZero = 0;
+
+(int DialPosition, int PointZero) RotateDial(int dial, int currentPosition)
+{
+    int zero = 0;
+
+    if (dial > 99)
+    {
+        dial -= 100;
+        zero++;
+    }
+    else if (dial == 0)
+    {
+        dial = 0;
+        zero++;
+    }
+    else if (dial < 0)
+    {
+        dial = 100 - Math.Abs(dial);
+        if (currentPosition != 0)
+            zero++;
+    }
+    
+    return (dial, zero);
+}
+
+string[] rotations = File.ReadAllLines("sequences.txt");
+foreach (String rotation in rotations)
+{
+    String direction = rotation[..1];
+    int clicks = Convert.ToInt32(rotation[1..]);
+    if (clicks > 99)
+    {
+        nbZero += clicks/100;
+        clicks %= 100;
+    }
+    currentPosition = dial;
+    dial = (direction == "R") ? dial += clicks : dial -= clicks;
+    var sequence = RotateDial(dial, currentPosition);
+    dial = sequence.DialPosition;
+    nbZero += sequence.PointZero;
+    if (dial == 0)
+        stopZero++;
+}
+
+Console.WriteLine($"Dial pointed at zero {nbZero} times and was left pointing at zero {stopZero} times");
