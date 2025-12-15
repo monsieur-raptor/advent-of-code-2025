cbfa4472a50ad73d408e2efeae7c19582924ec88
Simon Agozzino <simon.agozzino@monemail.com>
2025-12-15 09:51:18 +0100

    refactor: make code even moar idiomatic

--- advent-3/Program.cs
+++ advent-3/Program.cs
@@ -1,13 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-static int getMaxJoltage(List<int> bank, int length, int maxJoltage, int iterations = 2, int startPosition = 0)
+static int getMaxJoltage(List<int> bank, int length, int iterations = 2, int startPosition = 0, int maxJoltage = 0)
 {
     for (int counter = iterations; counter > 0; counter--)
     {
         int digit = 9;
         while (digit > 0)
         {
-            bool maxFound = false;
+            bool isMaxRating = false;
             for (int i = startPosition; i < length - 1; i++)
             {
                 if (bank[i] == digit)
@@ -15,12 +15,12 @@ static int getMaxJoltage(List<int> bank, int length, int maxJoltage, int iterati
                     maxJoltage += bank[i] * Convert.ToInt32(Math.Pow(10, counter - 1));
                     startPosition = i;
                     bank.RemoveAt(i);
-                    maxFound = true;
+                    isMaxRating = true;
                     break;
                 }
             }
             digit--;
-            if (maxFound)
+            if (isMaxRating)
                 break;
         }
     }
@@ -33,7 +33,6 @@ string[] banks= File.ReadAllLines("banks.txt");
 foreach (String bank in banks)
 {
     List<int> currentBank = new List<int>();
-    int maxJoltage = 0;
     int length = 0;
 
     for (int i = 0; i < bank.Length; i++)
@@ -41,7 +40,7 @@ foreach (String bank in banks)
         currentBank.Add(bank[i]-'0');
         length++;
     }
-    joltage += getMaxJoltage(currentBank, length, maxJoltage);
+    joltage += getMaxJoltage(currentBank, length);
 }
 
 Console.WriteLine(joltage);
